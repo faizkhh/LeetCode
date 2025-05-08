3772f076ac506da2fa239a65347595e9b43a3df0
Mohammad Faiz Alam <49671260+faizkhh@users.noreply.github.com>
2025-05-09 01:49:15 +0530

    Time: 1434 ms (39.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    u.user_id AS buyer_id,
+    u.join_date,
+    COALESCE(COUNT(o.order_id), 0) AS orders_in_2019
+FROM 
+    Users u
+LEFT JOIN 
+    Orders o 
+    ON u.user_id = o.buyer_id AND YEAR(o.order_date) = 2019
+GROUP BY 
+    u.user_id, u.join_date;
